d4ade852f35357242fc552c805f52d5730d9dbd7
Jimmy <nunu3041@gmail.com>
2021-03-23 01:27:39 +0900

    upload artifacts test

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
+__title__ = 'action-test'
+__version__ = '2021.03.22'
+
 def main():
     print('Hello world!')
     print('Version 5!')
 
 
 if __name__ == "__main__":
+    print(__title__)
     main()
